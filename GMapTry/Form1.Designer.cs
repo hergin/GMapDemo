a9be8e6908392acc7f82c5d4d37c54aadd140c66
hergin <hergin@crimson.ua.edu>
2016-09-23 08:14:06 -0500

    Add project files.

---
+++ GMapTry/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace GMapTry
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gmap = new GMap.NET.WindowsForms.GMapControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textbox_location1 = new System.Windows.Forms.TextBox();
+            this.textbox_location2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label_distance = new System.Windows.Forms.Label();
+            this.radioButton_fastest = new System.Windows.Forms.RadioButton();
+            this.radioButton_shortest = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // gmap
+            // 
+            this.gmap.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gmap.Bearing = 0F;
+            this.gmap.CanDragMap = true;
+            this.gmap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gmap.GrayScaleMode = false;
+            this.gmap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gmap.LevelsKeepInMemmory = 5;
+            this.gmap.Location = new System.Drawing.Point(12, 12);
+            this.gmap.MarkersEnabled = true;
+            this.gmap.MaxZoom = 18;
+            this.gmap.MinZoom = 2;
+            this.gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gmap.Name = "gmap";
+            this.gmap.NegativeMode = false;
+            this.gmap.PolygonsEnabled = true;
+            this.gmap.RetryLoadTile = 0;
+            this.gmap.RoutesEnabled = true;
+            this.gmap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gmap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gmap.ShowTileGridLines = false;
+            this.gmap.Size = new System.Drawing.Size(624, 467);
+            this.gmap.TabIndex = 0;
+            this.gmap.Zoom = 10D;
+            this.gmap.OnMarkerClick += new GMap.NET.WindowsForms.MarkerClick(this.gmap_OnMarkerClick);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(643, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Location1";
+            // 
+            // textbox_location1
+            // 
+            this.textbox_location1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textbox_location1.Location = new System.Drawing.Point(646, 30);
+            this.textbox_location1.Name = "textbox_location1";
+            this.textbox_location1.Size = new System.Drawing.Size(158, 20);
+            this.textbox_location1.TabIndex = 2;
+            // 
+            // textbox_location2
+            // 
+            this.textbox_location2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textbox_location2.Location = new System.Drawing.Point(646, 79);
+            this.textbox_location2.Name = "textbox_location2";
+            this.textbox_location2.Size = new System.Drawing.Size(158, 20);
+            this.textbox_location2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(643, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Location2";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(646, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Find Path";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(646, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Distance";
+            // 
+            // label_distance
+            // 
+            this.label_distance.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_distance.AutoSize = true;
+            this.label_distance.Location = new System.Drawing.Point(701, 181);
+            this.label_distance.Name = "label_distance";
+            this.label_distance.Size = new System.Drawing.Size(30, 13);
+            this.label_distance.TabIndex = 7;
+            this.label_distance.Text = "0 km";
+            // 
+            // radioButton_fastest
+            // 
+            this.radioButton_fastest.AutoSize = true;
+            this.radioButton_fastest.Checked = true;
+            this.radioButton_fastest.Location = new System.Drawing.Point(660, 111);
+            this.radioButton_fastest.Name = "radioButton_fastest";
+            this.radioButton_fastest.Size = new System.Drawing.Size(59, 17);
+            this.radioButton_fastest.TabIndex = 8;
+            this.radioButton_fastest.TabStop = true;
+            this.radioButton_fastest.Text = "Fastest";
+            this.radioButton_fastest.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_shortest
+            // 
+            this.radioButton_shortest.AutoSize = true;
+            this.radioButton_shortest.Location = new System.Drawing.Point(725, 111);
+            this.radioButton_shortest.Name = "radioButton_shortest";
+            this.radioButton_shortest.Size = new System.Drawing.Size(64, 17);
+            this.radioButton_shortest.TabIndex = 9;
+            this.radioButton_shortest.TabStop = true;
+            this.radioButton_shortest.Text = "Shortest";
+            this.radioButton_shortest.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 491);
+            this.Controls.Add(this.radioButton_shortest);
+            this.Controls.Add(this.radioButton_fastest);
+            this.Controls.Add(this.label_distance);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textbox_location2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textbox_location1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gmap);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gmap;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textbox_location1;
+        private System.Windows.Forms.TextBox textbox_location2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label_distance;
+        private System.Windows.Forms.RadioButton radioButton_fastest;
+        private System.Windows.Forms.RadioButton radioButton_shortest;
+    }
+}
+
---
+++ GMapTry/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.MapProviders;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+
+namespace GMapTry
+{
+    public partial class Form1 : Form
+    {
+        GMapOverlay route = new GMapOverlay("route");
+
+        public Form1()
+        {
+            InitializeComponent();
+            next = textbox_location1;
+            next.BackColor = Color.Yellow;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            List<PointLatLng> tempList = new List<PointLatLng>();
+            tempList.Add(new PointLatLng(5, 5));
+            tempList.Add(new PointLatLng(1, 1));
+            tempList.Add(new PointLatLng(3, 3));
+            tempList.Add(new PointLatLng(4, 4));
+            tempList.Add(new PointLatLng(0, 0));
+
+            sort(tempList);
+
+            gmap.MapProvider = GoogleMapProvider.Instance;
+            GMaps.Instance.Mode = AccessMode.ServerOnly;
+            gmap.ShowCenter = false;
+
+
+            GMapOverlay I20 = new GMapOverlay("I20");
+
+            string[] lines = Properties.Resources.I20.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            Bitmap bmp = new Bitmap(Properties.Resources.dot, new Size(5, 5));
+            foreach (string line in lines)
+            {
+                string[] langLong = line.Split(new char[] { ',' });
+                GMapMarker marker =
+                new GMarkerGoogle(
+                    new PointLatLng(double.Parse(langLong[0]), double.Parse(langLong[1])),
+                    bmp);
+                marker.ToolTipText = "I20 at " + line;
+                I20.Markers.Add(marker);
+            }
+
+            GMapOverlay I65 = new GMapOverlay("I65");
+
+            lines = Properties.Resources.I65.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string line in lines)
+            {
+                string[] langLong = line.Split(new char[] { ',' });
+                GMapMarker marker =
+                new GMarkerGoogle(
+                    new PointLatLng(double.Parse(langLong[0]), double.Parse(langLong[1])),
+                    bmp);
+                marker.ToolTipText = "I65 at " + line;
+                I65.Markers.Add(marker);
+            }
+
+            GMapOverlay I459 = new GMapOverlay("I459");
+
+            lines = Properties.Resources.I459.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string line in lines)
+            {
+                string[] langLong = line.Split(new char[] { ',' });
+                GMapMarker marker =
+                new GMarkerGoogle(
+                    new PointLatLng(double.Parse(langLong[0]), double.Parse(langLong[1])),
+                    bmp);
+                marker.ToolTipText = "I459 at " + line;
+                I459.Markers.Add(marker);
+            }
+
+            GMapOverlay I59 = new GMapOverlay("I59");
+
+            lines = Properties.Resources.I59.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string line in lines)
+            {
+                string[] langLong = line.Split(new char[] { ',' });
+                GMapMarker marker =
+                new GMarkerGoogle(
+                    new PointLatLng(double.Parse(langLong[0]), double.Parse(langLong[1])),
+                    bmp);
+                marker.ToolTipText = "I59 at " + line;
+                I59.Markers.Add(marker);
+            }
+
+            GMapOverlay I22 = new GMapOverlay("I22");
+
+            lines = Properties.Resources.I22.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string line in lines)
+            {
+                string[] langLong = line.Split(new char[] { ',' });
+                GMapMarker marker =
+                new GMarkerGoogle(
+                    new PointLatLng(double.Parse(langLong[0]), double.Parse(langLong[1])),
+                    bmp);
+                marker.ToolTipText = "I22 at " + line;
+                I22.Markers.Add(marker);
+            }
+
+            Bitmap hotspotBmp = new Bitmap(Properties.Resources.hotspot, new Size(50, 50));
+
+
+            GMapOverlay hotspots = new GMapOverlay("hotspots");
+            GMapMarker hotspot =
+                new GMarkerGoogle(
+                    new PointLatLng(33.8200890, -87.3135490),
+                    hotspotBmp);
+            hotspot.ToolTipText = "Hotspot";
+            hotspots.Markers.Add(hotspot);
+
+            hotspot =
+                new GMarkerGoogle(
+                    new PointLatLng(33.4441220, -86.9580590),
+                    hotspotBmp);
+            hotspot.ToolTipText = "Hotspot";
+            hotspots.Markers.Add(hotspot);
+
+            hotspot =
+                new GMarkerGoogle(
+                    new PointLatLng(33.4824330, -86.7090960),
+                    hotspotBmp);
+            hotspot.ToolTipText = "Hotspot";
+            hotspots.Markers.Add(hotspot);
+
+            hotspot =
+                new GMarkerGoogle(
+                    new PointLatLng(32.9834940, -86.6906810),
+                    hotspotBmp);
+            hotspot.ToolTipText = "Hotspot";
+            hotspots.Markers.Add(hotspot);
+
+
+            Bitmap vehicleBmp = new Bitmap(Properties.Resources.vehicle, new Size(50, 50));
+
+
+            GMapOverlay vehicles = new GMapOverlay("vehicles");
+            GMapMarker vehicle =
+                new GMarkerGoogle(
+                    new PointLatLng(33.3784590, -86.8201040),
+                    vehicleBmp);
+            vehicle.ToolTipText = "Vehicle";
+            vehicles.Markers.Add(vehicle);
+
+            vehicle =
+                new GMarkerGoogle(
+                    new PointLatLng(33.6742790, -86.5461920),
+                    vehicleBmp);
+            vehicle.ToolTipText = "Vehicle";
+            vehicles.Markers.Add(vehicle);
+
+
+            gmap.Overlays.Add(I20);
+            gmap.Overlays.Add(I65);
+            gmap.Overlays.Add(I459);
+            gmap.Overlays.Add(I59);
+            gmap.Overlays.Add(I22);
+            gmap.Overlays.Add(route);
+
+            gmap.Overlays.Add(hotspots);
+
+            gmap.Overlays.Add(vehicles);
+
+            // GMapOverlay routes = new GMapOverlay("roads");
+            // List<PointLatLng> points = new List<PointLatLng>();
+            // string[] lines = Properties.Resources.I20.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            // Array.Sort(lines);
+            // foreach (string line in lines)
+            // {
+            //     string[] langLong = line.Split(new char[] { ',' });
+            //     points.Add(new PointLatLng(double.Parse(langLong[0]), double.Parse(langLong[1])));
+            // }
+            // sort(points);
+            // GMapRoute route = new GMapRoute(points, "I20");
+            // route.Stroke = new Pen(Color.Red, 3);
+            // routes.Routes.Add(route);
+            // gmap.Overlays.Add(routes);
+
+            gmap.SetPositionByKeywords("Birmingham, AL");
+            //gmap.Position = new PointLatLng(33.518336, -86.825135);
+        }
+
+        void sort(List<PointLatLng> points)
+        {
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                double closestOffset = Double.MaxValue;
+                int index = i;
+                for (int j = i + 1; j < points.Count; j++)
+                {
+                    double d = distance(points[i], points[j]);
+                    if (d < closestOffset)
+                    {
+                        index = j;
+                        closestOffset = d;
+                    }
+                }
+                PointLatLng temp = new PointLatLng(points[i + 1].Lat, points[i + 1].Lng);
+                points[i + 1] = new PointLatLng(points[index].Lat, points[index].Lng);
+                points[index] = new PointLatLng(temp.Lat, temp.Lng); ;
+            }
+        }
+
+        double distance(PointLatLng p1, PointLatLng p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.Lat - p2.Lat, 2) * Math.Pow(p1.Lng - p2.Lng, 2));
+        }
+
+        TextBox next;
+        private void gmap_OnMarkerClick(GMapMarker item, MouseEventArgs e)
+        {
+            next.BackColor = Color.White;
+
+            next.Text = item.Position.Lat + "," + item.Position.Lng;
+            if (next == textbox_location1)
+                next = textbox_location2;
+            else
+                next = textbox_location1;
+
+            next.BackColor = Color.Yellow;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textbox_location1.Text) || string.IsNullOrEmpty(textbox_location2.Text))
+            {
+                MessageBox.Show("Please select two locations at least!", "Empty location");
+                return;
+            }
+            string[] location1 = textbox_location1.Text.Split(new char[] { ',' });
+            string[] location2 = textbox_location2.Text.Split(new char[] { ',' });
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(@"http://www.yournavigation.org/api/dev/route.php?flat=" + location1[0] + "&flon=" + location1[1] + "&tlat=" + location2[0] + "&tlon=" + location2[1] + "&v=motorcar&fast=" + (radioButton_fastest.Checked ? "1" : "0") + "&layer=mapnik&instructions=0");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+
+            HttpResponseMessage response = await client.GetAsync("");
+
+            String result = await response.Content.ReadAsStringAsync();
+            label_distance.Text = Regex.Match(result.Replace('\n', ' ').Replace('\r', ' '), "<distance>(.*?)</distance>").Groups[1].Value + " km";
+
+            String coordinates = Regex.Match(result.Replace('\n', ' ').Replace('\r', ' '), "<coordinates>(.*?)</coordinates>").Groups[1].Value;
+            Bitmap bmp = new Bitmap(Properties.Resources.dotGreen, new Size(15, 15));
+            route.Markers.Clear();
+            foreach (string coordinate in coordinates.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                string[] langLong = coordinate.Split(new char[] { ',' });
+                GMapMarker marker =
+                new GMarkerGoogle(
+                    new PointLatLng(double.Parse(langLong[0]), double.Parse(langLong[1])),
+                    bmp);
+                route.Markers.Add(marker);
+            }
+            gmap.Overlays.Remove(route);
+            gmap.Overlays.Add(route);
+            gmap.Refresh();
+            gmap.ReloadMap();
+        }
+    }
+}
